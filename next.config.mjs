26d95b0798870e87373a71d41a8601379d5f0a98
mido1983 <michael.doroshenko1@gmail.com>
2025-10-05 12:10:24 +0300

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,19 @@
 ﻿const csp = [
   "default-src 'self';",
-  "script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com;",
+  // Разрешаем Turnstile, Cloudflare, Vercel Live и Insights
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://vercel.live https://*.vercel-insights.com;",
   "style-src 'self' 'unsafe-inline';",
-  "img-src 'self' data: blob: https://challenges.cloudflare.com;",
-  "connect-src 'self' https://challenges.cloudflare.com https://*.vercel-insights.com;",
-  "frame-src 'self' https://challenges.cloudflare.com;",
+  "img-src 'self' data: blob: https://challenges.cloudflare.com https://vercel.live https://*.vercel-insights.com;",
+  "connect-src 'self' https://challenges.cloudflare.com https://vercel.live https://*.vercel-insights.com;",
+  "frame-src 'self' https://challenges.cloudflare.com https://vercel.live;",
   "base-uri 'self';",
   "form-action 'self';",
-  "frame-ancestors 'none'",
+  "frame-ancestors 'none';",
 ].join(' ');
 
 const nextConfig = {
   experimental: {
-    turbo: {
-      rules: {},
-    },
+    turbo: { rules: {} },
   },
   reactStrictMode: true,
   trailingSlash: false,
